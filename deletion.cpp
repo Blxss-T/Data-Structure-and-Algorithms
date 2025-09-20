7a0ee406c573964e595181da140dd84d451a7240
Bless <tetaangelbless014@gmail.com>
2025-09-20 22:41:55 +0200

    Initial commit

--- deletion.cpp
+++ deletion.cpp
@@ -1,14 +1,14 @@
-# include <iostream>
-using namespace std;
- int main() {
-     int scores[]={52,78,75,68,88,63,75,90,78};
-     int len = sizeof(scores)/sizeof(scores[0]);
-     for (int i=3;i<len;i++) {
-         scores[i]=scores[i+1];
-     }
-     len--;
-     for (int i=0;i<len;i++) {
-         cout<<scores[i]<<endl;
-     }
-     return 0;
- }
+// # include <iostream>
+// using namespace std;
+//  int main() {
+//      int scores[]={52,78,75,68,88,63,75,90,78};
+//      int len = sizeof(scores)/sizeof(scores[0]);
+//      for (int i=3;i<len;i++) {
+//          scores[i]=scores[i+1];
+//      }
+//      len--;
+//      for (int i=0;i<len;i++) {
+//          cout<<scores[i]<<endl;
+//      }
+//      return 0;
+//  }
