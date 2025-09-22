5ecb12debc3910a9332a58fa6b5a105ac8da997c
Bless <tetaangelbless014@gmail.com>
2025-09-22 13:24:28 +0200

    Initial commit

---
+++ arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int marks [3][0]={
+    {80,75,76,75},
+    {54,72,55,70},
+    {65,85,35,59}
+    };
+
+    for(int i=0; i<3; i++) {
+        for (int j=0; j<4; j++) {
+            cout<<marks[i][j]<<" "<<endl;
+        }
+    }
+}
---
+++ cmake-build-debug/even.cpp
@@ -0,0 +1,11 @@
+// #include <iostream>
+//  using namespace std;
+//
+// int main() {
+//  int sum=0;
+//  int a=0;
+//  for (int a=0; a <100;a+=2) {
+//   sum+=a;
+//  }
+// cout<<sum;
+// }
