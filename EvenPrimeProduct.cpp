cc008c628222865f3d4789c8c3c6fa89bd21bdfe
Bless <tetaangelbless014@gmail.com>
2025-09-26 13:53:51 +0200

    Initial commit

---
+++ EvenPrimeProduct.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main () {
+    int n;
+    int product = 1;
+    for (int i=2; i<100; i++) {
+        bool isPrime = true;
+        for (int j=2; j<i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            product *= i;
+        }
+    }
+    cout << product << endl;
+}
