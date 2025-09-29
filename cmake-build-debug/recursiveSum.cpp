8e394e340890617766d2cf65b484be47cfeb2892
Bless <tetaangelbless014@gmail.com>
2025-09-29 12:14:15 +0200

    Initial commit

---
+++ cmake-build-debug/recursiveSum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ int sum(int n) {
+     if (n == 1)
+         return 1;
+     int smallOutput=sum(n-1);
+     return n+smallOutput;
+ }
+int main() {
+     int n;
+     cout<< "Enter a number:"<<endl;
+     cin>>n;
+     int result = sum(n);
+     cout<<result<<endl;
+ }
