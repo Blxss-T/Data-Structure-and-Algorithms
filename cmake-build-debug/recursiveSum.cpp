40aae6ba4e08741dbe05ef3210f1ee750f6bb029
Bless <tetaangelbless014@gmail.com>
2025-09-30 23:47:26 +0200

    Initial commit

--- cmake-build-debug/recursiveSum.cpp
+++ cmake-build-debug/recursiveSum.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-using namespace std;
- int sum(int n) {
-     if (n == 1)
-         return 1;
-     int smallOutput=sum(n-1);
-     return n+smallOutput;
- }
-int main() {
-     int n;
-     cout<< "Enter a number:"<<endl;
-     cin>>n;
-     int result = sum(n);
-     cout<<result<<endl;
- }
+// #include <iostream>
+// using namespace std;
+//  int sum(int n) {
+//      if (n == 1)
+//          return 1;
+//      int smallOutput=sum(n-1);
+//      return n+smallOutput;
+//  }
+// int main() {
+//      int n;
+//      cout<< "Enter a number:"<<endl;
+//      cin>>n;
+//      int result = sum(n);
+//      cout<<result<<endl;
+//  }
---
+++ recursivePrint.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int recursivePrint(int arr[],int n) {
+    if (n<0) return 0;
+    cout<<arr[n]<<endl;
+    return recursivePrint(arr,n-1);
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int n = size -1;
+    recursivePrint(arr,n);
+
+
+}
