e73d5e140706982a4e5792ad19e3fbf0826c0970
Bless <tetaangelbless014@gmail.com>
2025-10-07 12:00:32 +0200

    Polished inheritance and constructors

--- classCircle.cpp
+++ classCircle.cpp
@@ -1,25 +1,25 @@
-#include <iostream>
-
-using namespace std;
-class circle {
-private:
-    double radius;
-    double PI=3.172;
-public:
-    circle(double r) {
-        this->radius=r;
-    }
-
-    void area() {
-        cout<<"The area of the circle is "<<(radius*radius)*PI<<endl;
-    }
-    void circumference() {
-        cout<<"The circumference of the circle is "<<(2*radius)*PI<<endl;
-    }
-
-};
-int main() {
-    circle c1(5);
-    c1.area();
-    c1.circumference();
-}
+// #include <iostream>
+//
+// using namespace std;
+// class circle {
+// private:
+//     double radius;
+//     double PI=3.172;
+// public:
+//     circle(double r) {
+//         this->radius=r;
+//     }
+//
+//     void area() {
+//         cout<<"The area of the circle is "<<(radius*radius)*PI<<endl;
+//     }
+//     void circumference() {
+//         cout<<"The circumference of the circle is "<<(2*radius)*PI<<endl;
+//     }
+//
+// };
+// int main() {
+//     circle c1(5);
+//     c1.area();
+//     c1.circumference();
+// }
---
+++ classPerson.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+class Person {
+private:
+    string name;
+    int age;
+public:
+Person(string n,int age) {
+    this->name =n;
+    this->age=age;
+}
+    void printMe() {
+    cout<<"My name is "<<name<<" and I am "<<age<<"years old"<<endl;
+    cout<<"I am a person.....simply";
+}
+};
+class Student :public Person {
+    string school;
+    string className;
+    public :
+        Student(string n,int a,string school,string cName): Person(n,a) {
+        this->school=school;
+        this->className=cName;
+    }
+    void printStudent() {
+        cout<<"Hey there, It is us against the world right? all the way from "<<school<<"class: "<<className<<endl;
+    }
+};
+int main() {
+    Person p1("Bless",18);
+    p1.printMe();
+    Student s1("Bless",18,"RCA","Y2C");
+    s1.printStudent();
+    s1.printMe();
+}
