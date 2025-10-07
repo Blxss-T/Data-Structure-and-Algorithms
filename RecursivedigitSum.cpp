0bcc44551fa587652fcd43fc61b21e5067685583
Bless <tetaangelbless014@gmail.com>
2025-10-07 07:43:34 +0200

    Initial commit

--- RecursivedigitSum.cpp
+++ RecursivedigitSum.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-using namespace std;
-int recursiveDigitSum(int n) {
-    if (n == 0) return 0;
-    return n%10 + recursiveDigitSum(n/10);
-
-}
-int main() {
-    cout<<recursiveDigitSum(5489);
-
-}
+// #include <iostream>
+// using namespace std;
+// int recursiveDigitSum(int n) {
+//     if (n == 0) return 0;
+//     return n%10 + recursiveDigitSum(n/10);
+//
+// }
+// int main() {
+//     cout<<recursiveDigitSum(5489);
+//
+// }
--- oop.cpp
+++ oop.cpp
@@ -1,3 +1,20 @@
-//
-// Created by HP on 06/10/2025.
-//
+#include <iostream>
+using namespace std;
+class Person {
+public:
+    string name;
+    int age;
+    void display() {
+        cout <<"Life is lifing"<<endl;
+    }
+};
+class Student : public Person {
+
+}
+int main() {
+    Person p;
+    p.name = "John";
+    p.age = 18;
+    cout<<"My name is: "<<p.name<<" "<<"I am :"<<p.age<<"years old"<<endl;
+    p.display();
+}
