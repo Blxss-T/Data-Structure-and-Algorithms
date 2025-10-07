9b1cd61b0b082c5c03f928e18464d962905d88c6
Bless <tetaangelbless014@gmail.com>
2025-10-07 09:55:18 +0200

    initial commit

--- timeComplex1.cpp
+++ timeComplex1.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
-using namespace std;
-
-int fun2(int n) {
-    int m = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            m += 1;
-        }
-    }
-    return m;
-}
-
-// Testing Code
-int main() {
-    cout << "N = 100, Number of instructions (O(n^2)): " << fun2(100) << endl;
-    return 0;
-}
+// #include <iostream>
+// using namespace std;
+//
+// int fun2(int n) {
+//     int m = 0;
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             m += 1;
+//         }
+//     }
+//     return m;
+// }
+//
+// // Testing Code
+// int main() {
+//     cout << "N = 100, Number of instructions (O(n^2)): " << fun2(100) << endl;
+//     return 0;
+// }
