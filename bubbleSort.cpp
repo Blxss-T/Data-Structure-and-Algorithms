28d03c56c684f26705aadf6599437a9b47343620
Bless <tetaangelbless014@gmail.com>
2025-10-20 11:33:21 +0200

    Initial commit

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
-int main () {
-    int a[5]={50,40,20,30,10};
+void bubbleSort(int a[],int n) {
     cout<<"Display before sorting";
     for (int k=0;k<n;k++) {
         cout<<a[k]<<" ";
     }
-    cout<<
+
       for (int i=0;i<n-1;i++) {
-          for (int j=0;j<n-1;j+=) {
+          for (int j=0;j<n-1;j++) {
               int temp;
               if (a[j]>a[j+1]) {
                   temp =a[j];
@@ -22,4 +21,9 @@ int main () {
     for (int t=0;t<n;t++) {
         cout<<a[t];
     }
+}
+int main () {
+    int n=5;
+    int a[n]={50,40,20,30,10};
+
 }
