3dee16a57ce8e26270efa8f60565c09af22077bd
Bless <tetaangelbless014@gmail.com>
2025-10-20 11:25:02 +0200

    Initialised bubble sorting  algorithm

---
+++ bubbleSort.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int a[5]={50,40,20,30,10};
+    for (int k=0;k<n;k++) {
+        cout<<a[k]<<" ";
+    }
+
+
+    for (int t=0;t<n;t++)
+}
