a3fedfb7fb215c18491a802a73c88cb6866394e2
Bless <tetaangelbless014@gmail.com>
2025-10-20 11:25:24 +0200

    Initial commit

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,7 +5,17 @@ int main () {
     for (int k=0;k<n;k++) {
         cout<<a[k]<<" ";
     }
-
+    cout<<
+      for (int i=0;i<n-1;i++) {
+          for (int j=0;j<n-1;j+=) {
+              int temp;
+              if (a[j]>a[j+1]) {
+                  temp =a[j];
+                  a[j]=a[j+1];
+                  a[j+1]=temp;
+              }
+          }
+      }
 
     for (int t=0;t<n;t++)
 }
