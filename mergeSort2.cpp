9db1fa2ab0848ceb358d1cde9d4264137dfba575
Bless <tetaangelbless014@gmail.com>
2025-10-21 11:07:52 +0200

    Initial commit

--- mergeSort2.cpp
+++ mergeSort2.cpp
@@ -1,3 +1,35 @@
-//
-// Created by HP on 21/10/2025.
-//
+#include <iostream>
+using namespace std;
+void mergeArrays(int x[],int y[],int a,itn s ,int e) {
+    int mid=(s+e)/2;
+    int i=s;
+    int j=mid+1;
+    int k=s;
+    while (i<=mid&&j<=e) {
+        if (x[i]<y[j]) {
+            a[k]=x[i];
+            i++;
+            k++;
+        }else {
+            a[k]=y[j];
+            j++;
+            k++;
+        }
+    }
+    while (i<=mid) {
+        a[k]=x[i];
+        k++;
+        i++;
+    }
+    while (j<=e) {
+        a[k]=y[j];
+        k++;
+        j++;
+    }
+}
+void mergeSort(int a[],int s,int e) {
+    if (s>=e) {
+        return;
+    }
+
+}
