29887c850e218f4fe7ce0da0b0f77a410d973001
Bless <tetaangelbless014@gmail.com>
2025-10-29 07:53:15 +0200

    Initial commit

--- binaryRecursive.cpp
+++ binaryRecursive.cpp
@@ -1,3 +1,19 @@
-//
-// Created by HP on 29/10/2025.
-//
+#include <iostream>
+using namespace std;
+int binary_function(int a[] ,int low,int high, int target) {
+    int mid=(low+high)/2;
+    if (low>high)
+        return -1;
+
+        if (a[mid]==target)
+            return mid;
+
+        else if (a[mid]>target)
+            return binary_function(a,low,mid-1,target);
+
+        else
+            return binary_function(a,mid+1);
+
+    }
+int main () {
+}
--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,34 +1,34 @@
-#include<iostream>
-using namespace std;
-int BinarySearch(int a[],int n,int key) {
-    int s =0;
-    int e=n-1;
-    while (s<=e) {
-        int mid=(s+e)/2;
-        if (a[mid]==key) {
-            return mid;
-        }
-        else if (a[mid]>key){
-            e=mid-1;
-        }
-        else {
-            s=mid+1;
-        }
-    }
-    return -1;
-
-}
-int main () {
-    int n;
-    cin>>n;
-    int a[n];
-    for (int i=0;i<n;i++) {
-        cin>>a[i];
-    }
-    cout<<"Enter key"<<endl;
-    int key;
-    cin>>key;
-    sort(a,a+1);
-
-
-}
+// #include<iostream>
+// using namespace std;
+// int BinarySearch(int a[],int n,int key) {
+//     int s =0;
+//     int e=n-1;
+//     while (s<=e) {
+//         int mid=(s+e)/2;
+//         if (a[mid]==key) {
+//             return mid;
+//         }
+//         else if (a[mid]>key){
+//             e=mid-1;
+//         }
+//         else {
+//             s=mid+1;
+//         }
+//     }
+//     return -1;
+//
+// }
+// int main () {
+//     int n;
+//     cin>>n;
+//     int a[n];
+//     for (int i=0;i<n;i++) {
+//         cin>>a[i];
+//     }
+//     cout<<"Enter key"<<endl;
+//     int key;
+//     cin>>key;
+//     sort(a,a+1);
+//
+//
+// }
