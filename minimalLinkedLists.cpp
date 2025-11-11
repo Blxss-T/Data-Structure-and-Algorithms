341b75922c3dda86b7b6f9d0d48ec9e4e1b07f78
Bless <tetaangelbless014@gmail.com>
2025-11-11 09:51:33 +0200

    Initial commit

--- minimalLinkedLists.cpp
+++ minimalLinkedLists.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-using namespace std;
-
-class Node {
-    public :
-        int data ;
-    Node *next;
-    Node (int d ) {
-        data=d;
-        next=nullptr;
-    }
-
-};
-void display(Node *head ) {
-    while (head!=nullptr) {
-        cout<<head->data<<"";
-        head =head->next;
-    }
-}
-int main () {
-    Node *n1 = new Node(10);
-    Node *n2 = new Node(20);
-    Node *n3 = new Node(30);
-    Node *n4 = new Node(40);
-
-    n1->next=n2;
-    n2->next=n3;
-    n3->next=n4;
-    n4->next=nullptr;
-    display(n1);
-    return 0;
-
-
-
-
-}
+// #include <iostream>
+// using namespace std;
+//
+// class Node {
+//     public :
+//         int data ;
+//     Node *next;
+//     Node (int d ) {
+//         data=d;
+//         next=nullptr;
+//     }
+//
+// };
+// void display(Node *head ) {
+//     while (head!=nullptr) {
+//         cout<<head->data<<"";
+//         head =head->next;
+//     }
+// }
+// int main () {
+//     Node *n1 = new Node(10);
+//     Node *n2 = new Node(20);
+//     Node *n3 = new Node(30);
+//     Node *n4 = new Node(40);
+//
+//     n1->next=n2;
+//     n2->next=n3;
+//     n3->next=n4;
+//     n4->next=nullptr;
+//     display(n1);
+//     return 0;
+//
+//
+//
+//
+// }
--- newLinkedList.cpp
+++ newLinkedList.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-using namespace std;
-
-class Student {
-public :
-    int code ;
-    string name;
-    string school;
-    Student *next;
-    Student(int c, string fname, string school) {
-        code =c;
-        name= fname;
-        school= school;
-        next = nullptr;
-    };
-    void display() {
-        while (head!=nullptr) {
-            cout<<head->code
-        }
-    }
-    int main () {
-        Student *s1=new Student(101,"Dog","RCA");
-        Student *s2=new Student(102,"Dogy","RCA");
-        Student *s3=new Student(103,"Doggy","RCA");
-        s1->next=s2;
-        s2->next=s3;
-        display(s1);
-
-    }
-
-
-
-
-
-};
+// #include <iostream>
+// using namespace std;
+//
+// class Student {
+// public :
+//     int code ;
+//     string name;
+//     string school;
+//     Student *next;
+//     Student(int c, string fname, string school) {
+//         code =c;
+//         name= fname;
+//         school= school;
+//         next = nullptr;
+//     };
+//     void display() {
+//         while (head!=nullptr) {
+//             cout<<head->code
+//         }
+//     }
+//     int main () {
+//         Student *s1=new Student(101,"Dog","RCA");
+//         Student *s2=new Student(102,"Dogy","RCA");
+//         Student *s3=new Student(103,"Doggy","RCA");
+//         s1->next=s2;
+//         s2->next=s3;
+//         display(s1);
+//
+//     }
+//
+//
+//
+//
+//
+// };
