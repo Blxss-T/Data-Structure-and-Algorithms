8d852482fa860d2e7f26e7f387fa7e780721cb18
Bless <tetaangelbless014@gmail.com>
2025-11-11 09:21:20 +0200

    Initial commit

--- binaryRecursive.cpp
+++ binaryRecursive.cpp
@@ -1,26 +1,27 @@
-#include <iostream>,
-using namespace std;
-int binaryRecursive(int a[] ,int low,int high, int target) {
-    int mid=(low+high)/2;
-    if (low>high)
-        return -1;
-
-        if (a[mid]==target)
-            return mid;
-
-        else if (a[mid]>target)
-            return binaryRecursive(a,low,mid-1,target);
-
-        else
-            return binaryRecursive(a,low,mid+1, target);
-
-    }
-int main () {
-    int a[]={1,2,3,4,5,6,6,7,8,9};
-    int low=0;
-    int n= sizeof(a)/sizeof(a[0]);
-    int high=n-1;
-    int target=8;
-    int targetValue =binaryRecursive(a,low,high,target);
-    cout<<a[targetValue]<<endl;
-}
+// #include <iostream>
+// using namespace std;
+// int binaryRecursive(int a[] ,int low,int high, int target) {
+//     int mid=(low+high)/2;
+//     if (low>high)
+//         return -1;
+//
+//         if (a[mid]==target)
+//             return mid;
+//
+//         else if (a[mid]>target)
+//             return binaryRecursive(a,low,mid-1,target);
+//
+//         else
+//             return binaryRecursive(a, mid + 1, high, target);
+//
+//     }
+// int main () {
+//     int a[]={1,2,3,4,5,6,6,7,8,9};
+//     int low=0;
+//     int n= sizeof(a)/sizeof(a[0]);
+//     int high=n-1;
+//     int target=8;
+//     cout<<binaryRecursive(a,low,high,target)<<endl;
+//     int targetValue =binaryRecursive(a,low,high,target);
+//     cout<<a[targetValue]<<endl;
+// }
---
+++ linkedLists.cpp
@@ -0,0 +1,35 @@
+// #include <iostream>
+// using namespace std;
+//
+// class Node {
+//     public :
+//         int data ;
+//     Node *next;
+//
+// };
+// void display(Node *head ) {
+//     while (head!=nullptr) {
+//         cout<<head->data<<"";
+//         head =head->next;
+//     }
+// }
+// int main () {
+//     Node *n1 = new Node();
+//     Node *n2 = new Node();
+//     Node *n3 = new Node();
+//     Node *n4 = new Node();
+//     n1->data=10;
+//     n1->next=n2;
+//     n2->data=20;
+//     n2->next=n3;
+//     n3->data=30;
+//     n3->next=n4;
+//     n4->data=10;
+//     n4->next=nullptr;
+//     display(n1);
+//     return 0;
+//
+//
+//
+//
+// }
