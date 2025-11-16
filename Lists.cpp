432922629ad2bd23ed37a6b820aef75ca0669ecd
Bless <tetaangelbless014@gmail.com>
2025-11-16 20:43:28 +0200

    Initial commit

--- Lists.cpp
+++ Lists.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 
 int main() {
+    //A simple list
+    int ages[]={3,4,5,6,3,2,};
+    ages[8]={79};
+    for (int i=0;i>=sizeof(ages);i++) {
+        cout<<(ages[i])<<endl;
+    }
+
+
+
 
 }
