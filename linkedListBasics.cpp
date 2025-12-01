e683c07407b5e992600f1411fc4f4336531ee253
Bless <tetaangelbless014@gmail.com>
2025-12-01 10:48:23 +0200

    Initial commit

--- linkedListBasics.cpp
+++ linkedListBasics.cpp
@@ -1,32 +1,35 @@
-#include<iostream>
-using namespace std;
-class Node {
-    // in class all is private by default
-public:
-    int data;
-    Node *next;
-    Node (int data) {
-        this->data =data;
-        this->next=NULL;
-    }
-
-};
-class LinkedList {
-    Node *head;
-public:
-    LinkedList() {
-        head=NULL;
-    }
-    void display() {
-        Node *temp=head;
-        while (head->next!=NULL) {
-            cout<<head
-        }
-    }
-};
-int main() {
-    Node *n1= new Node(10);
-    Node *n2= new Node(20);
-    n1->next=n2;
-    cout<<n1->data<<endl;
-}
+// #include<iostream>
+// using namespace std;
+// class Node {
+//     // in class all is private by default
+// public:
+//     int data;
+//     Node *next;
+//     Node (int data) {
+//         this->data =data;
+//         this->next=NULL;
+//     }
+//
+// };
+// class LinkedList {
+//     Node *head;
+// public:
+//     LinkedList() {
+//         head=NULL;
+//     }
+//     void display() {
+//         Node *temp=head;
+//         while (head->next!=NULL) {
+//             cout<<temp->data<<endl;
+//             temp=temp->next;
+//         }
+//     }
+// };
+// int main() {
+//     Node *n1= new Node(10);
+//     Node *n2= new Node(20);
+//     n1->next=n2;
+//     cout<<n1->data<<endl;
+//     LinkedList list;
+//     list.display();
+// }
