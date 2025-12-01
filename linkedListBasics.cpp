e6ce59e9c33bbbb2a7aab58ec02011455f6c30ec
Bless <tetaangelbless014@gmail.com>
2025-12-01 08:56:46 +0200

    Initial commit

---
+++ linkedListBasics.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Node {
+    // in class all is private by default
+public:
+    int data;
+    Node *next;
+    Node (int data) {
+        this->data =data;
+        this->next=NULL;
+    }
+
+};
+int main() {
+    Node *n1= new Node(10);
+    Node *n2= new Node(20);
+    n1->next=n2;
+    cout<<n1->data<<endl;
+}
--- linkedListpersonal.cpp
+++ linkedListpersonal.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-using namespace std;
-class Node {
-public:
-    int data;
-    Node* next;
-};
-void traverse(Node* head) {
-    Node* current=head;
-    while (current->next!=NULL) {
-        cout<<current->next<<"->"<<endl;
-    }
-
-}
-
- int main () {
-     Node* head=new Node();
-     Node* second = new Node();
-     Node* third = new Node();
-     head->data=10;
-     head->next=second;
-     second->data=20;
-     second->next=third;
-     third->data=30;
-     third->next=NULL;
-    traverse(head);
-
-
-    class Node {
-        int data;
-        Node *next;
-        Node(int data) {
-            this.data   =data;
-        }
-    };
+// #include <iostream>
+// using namespace std;
+// class Node {
+// public:
+//     int data;
+//     Node* next;
+// };
+// void traverse(Node* head) {
+//     Node* current=head;
+//     while (current->next!=NULL) {
+//         cout<<current->next<<"->"<<endl;
+//     }
+//
+// }
+//
+//  int main () {
+//      Node* head=new Node();
+//      Node* second = new Node();
+//      Node* third = new Node();
+//      head->data=10;
+//      head->next=second;
+//      second->data=20;
+//      second->next=third;
+//      third->data=30;
+//      third->next=NULL;
+//     traverse(head);
+//
+//
+//     class Node {
+//         int data;
+//         Node *next;
+//         Node(int data) {
+//             this.data   =data;
+//         }
+//     };
