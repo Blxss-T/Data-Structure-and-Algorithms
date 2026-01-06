71453d1a7d87c1f6e981619b312bf2aadfa23e5b
Bless <tetaangelbless014@gmail.com>
2026-01-06 21:42:31 +0200

    Initial commit

--- linkedListReview.cpp
+++ linkedListReview.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 using namespace std;
  class Node {
+ public:
      int data;
      Node* next;
+ public:
     Node (int value) {
         data=value;
         next=nullptr;
     }
  };
 class linkedList {
+private:
     Node* head;
+public:
     linkedList() {
         head=nullptr;
-
     }
 
-}
+    void insertAtEnd(int val) {
+        Node* newNode= new Node(val);
+        if (head== nullptr) {
+            head=newNode;
+            return;
+        }
+        Node* temp= head;
+        while (temp->next != nullptr) {
+            temp=temp->next;
+        }
+        temp->next=newNode;
+    }
+    void traverseLinkedList() {
+
+    }
+};
