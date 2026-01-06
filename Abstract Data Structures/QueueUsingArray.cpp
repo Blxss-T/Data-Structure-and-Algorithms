1ad522c1acc094bce9a7aa97b32b3eacedefc069
Bless <tetaangelbless014@gmail.com>
2026-01-06 09:55:37 +0200

    Initial commit

--- Abstract Data Structures/QueueUsingArray.cpp
+++ Abstract Data Structures/QueueUsingArray.cpp
@@ -1,3 +1,19 @@
-//
-// Created by HP on 06/01/2026.
-//
+#include <iostream>
+using namespace std;
+int n= 100;
+int arrQueue[n],front =-1,rear =-1;
+void enqueue(int value) {
+    if (rear == n-1) {
+        return;
+    }
+    if (front = -1) {
+        front ++;
+    }
+    rear = rear + 1;
+    arrQueue[rear]=value;
+}
+void display() {
+    for (int i=front;i<=rear;i++) {
+
+    }
+}
