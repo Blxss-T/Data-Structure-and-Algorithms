1542254b51b5c32a5e35358f5071e8e4ab9ca096
Bless <tetaangelbless014@gmail.com>
2026-01-07 19:41:27 +0200

    Initial commit

--- linkedListReview.cpp
+++ linkedListReview.cpp
@@ -30,7 +30,30 @@ public:
         }
         temp->next=newNode;
     }
+    void insertAtBegining(int val) {
+        Node* newNode= new Node(val);
+        if (head== nullptr) {
+            head=newNode;
+        }
+        newNode->next=head;
+        head = newNode;
+    }
+    void deleteAtBeginning() {
+        if (head==nullptr) {
+            return;
+        }
+        Node* temp= head;
+        head=temp->next;
+    }
     void traverseLinkedList() {
+        if (head== nullptr) {
+            cout<<"Is empty"<<endl;
+        }
+        Node* temp=head;
+        while (temp->next!= nullptr) {
+            cout<<temp->data<<"->";
+            temp= temp->next;
+        }
 
     }
 };
