4f221602068bde71ad050a2b1e74539f39b4df43
Bless <tetaangelbless014@gmail.com>
2026-01-07 21:40:42 +0200

    Initial commit

--- queuesPersonal .cpp
+++ queuesPersonal .cpp
@@ -1,3 +1,25 @@
-//
-// Created by HP on 07/01/2026.
-//
+#include <iostream>
+using namespace std;
+int n=10;
+int qArray[n],front =-1,rear=-1;
+void enqueue(int x) {
+    //check if its not full
+    if (rear==n-1) {
+        cout<<"Queue is full\n";
+        return;
+    }
+    if (front == -1) {
+        front =0;
+    }
+    rear++;
+    qArray[rear]=x;
+}
+void displayQueue() {
+    if (front==-1) {
+        cout<<"Queue is empty\n";
+        return;
+    }
+    for (int i= front;i<=rear;i++) {
+        cout<<
+    }
+}
