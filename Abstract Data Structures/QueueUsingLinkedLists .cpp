7b13ca5b8e42d711cc1b5d56bba6446ee20e2ff9
Bless <tetaangelbless014@gmail.com>
2026-01-21 11:05:47 +0200

    Initial commit

--- Abstract Data Structures/QueueUsingLinkedLists .cpp
+++ Abstract Data Structures/QueueUsingLinkedLists .cpp
@@ -1,5 +1,79 @@
 #include<iostream>
 using namespace std;
 class Node {
-
+public:
+    int data;
+    Node *next;
+    Node(int x) {
+        this -> data=x;
+        next =NULL;
+    }
 };
+class Queue {
+    Node*head;
+    Node * tail;
+    int size;
+public:
+    Queue() {
+        head= null ;
+        tail= null
+        size =0;
+    }
+    int getSize() {
+        return size;
+    }
+    int count () {
+        ...;
+    }
+    bool isEmpty() {
+        return  size==0;
+    }
+    void enqueue (int ele) {
+        Node * n = new Node(ele);
+        if (head == NULL) {
+            head = n;
+            tail=n;
+        }
+        else {
+            tail->next=n;
+            tail=n;
+        }
+        size ++;
+    }
+    void dequeue() {
+        if (isEmpty()) {
+            return;
+        }
+        Node * temp = head;
+        head = head ->next;
+        temp->next= NULL;
+
+        delete temp;
+        size --;
+    }
+    int front () {
+        if (isEmpty()) {
+            return 0 ;
+        }
+        return head->data;
+    }
+    void display() {
+        Node *temp= head ;
+        while (temp!=NULL) {
+            cout<<temp->data<<"";
+            temp=temp->next;
+        }
+    };
+    int main () {
+        Queue *q = new Queue();
+        q->enqueue(10);
+        q->enqueue(20);
+        q->enqueue(30);
+        q->enqueue(40);
+        q->display();
+        q->dequeue();
+        q->display();
+
+    }
+};
+
--- queuesPersonal .cpp
+++ queuesPersonal .cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-using namespace std;
-int n=10;
-int qArray[n],front =-1,rear=-1;
-void enqueue(int x) {
-    //check if its not full
-    if (rear==n-1) {
-        cout<<"Queue is full\n";
-        return;
-    }
-    if (front == -1) {
-        front =0;
-    }
-    rear++;
-    qArray[rear]=x;
-}
-void displayQueue() {
-    if (front==-1) {
-        cout<<"Queue is empty\n";
-        return;
-    }
-    for (int i= front;i<=rear;i++) {
-        cout<<qArray[i]<<" ";
-    }
-    cout<<endl;
-}
-void dequeue() {
-    if (front==-1||front >rear) {
-        cout<<"Queue is empty\n";
-        return;
-    }
-    front++;
-}
+// #include <iostream>
+// using namespace std;
+// int n=10;
+// int qArray[n],front =-1,rear=-1;
+// void enqueue(int x) {
+//     //check if its not full
+//     if (rear==n-1) {
+//         cout<<"Queue is full\n";
+//         return;
+//     }
+//     if (front == -1) {
+//         front =0;
+//     }
+//     rear++;
+//     qArray[rear]=x;
+// }
+// void displayQueue() {
+//     if (front==-1) {
+//         cout<<"Queue is empty\n";
+//         return;
+//     }
+//     for (int i= front;i<=rear;i++) {
+//         cout<<qArray[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+// void dequeue() {
+//     if (front==-1||front >rear) {
+//         cout<<"Queue is empty\n";
+//         return;
+//     }
+//     front++;
+// }
