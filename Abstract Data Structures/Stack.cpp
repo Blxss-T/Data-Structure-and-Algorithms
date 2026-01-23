98241d2be322334636a9e44358534625eee7d2eb
Bless <tetaangelbless014@gmail.com>
2026-01-23 14:05:01 +0200

    Initial commit

--- Abstract Data Structures/Stack.cpp
+++ Abstract Data Structures/Stack.cpp
@@ -1,3 +1,42 @@
-//
-// Created by HP on 21/01/2026.
-//
+#include <iostream>
+using namespace std;
+template <typename T>
+class Node {
+    T data;
+    Node <T> * next;
+public:
+    Node (Tdata) {
+        this ->data = data;
+        next=NULL;
+    }
+};
+template <typename T>
+class Stack {
+    Node <T> *head;
+    int size ;
+public:
+    Stack() {
+        head= NULL;
+        size=0;
+    }
+    bool isEmpty() {
+        if (head== nullptr) {
+            return true;
+        }
+    }
+    void push(T ele) {
+        Node<T> * newNode= new Node<T>;
+        if (head == nullptr) {
+            head =newNode;
+        }
+        else {
+
+
+
+
+
+
+            
+        }
+    }
+};
