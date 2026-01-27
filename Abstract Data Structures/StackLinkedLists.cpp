8cd8056ec96f2dcd974edb23d7dd510433222c23
Bless <tetaangelbless014@gmail.com>
2026-01-27 21:18:00 +0200

    Initial commit

--- Abstract Data Structures/StackLinkedLists.cpp
+++ Abstract Data Structures/StackLinkedLists.cpp
@@ -1,3 +1,50 @@
-//
-// Created by HP on 27/01/2026.
-//
+#include<iostream>
+using namespace std;
+class  Node{
+public:
+    int value;
+    Node * next;
+public:
+    Node (int val) {
+        this->value=val;
+        this->next=nullptr;
+    }
+};
+class linkedList {
+    Node * head;
+    linkedList() {
+        head=nullptr;
+    }
+
+    void addNodeStack(int value) {
+        Node* newNode= new Node(value);
+        if (head== nullptr) {
+            head=newNode;
+            return;
+        }
+        Node* temp=head;
+        while (temp->next!=nullptr) {
+            temp= temp->next;
+        }
+        temp->next=newNode;
+    }
+
+    void pop(int val) {
+        if (head==nullptr) {
+            cout<<"Stack underflow";
+            return;
+        }
+        // if (head->value==val) {
+        //     Node*temp=head;
+        //     head=head->next;
+        //     delete temp;
+        //     return;
+        // }
+        Node* temp=head;
+        while (temp->next->next!=nullptr) {
+            temp= temp->next;
+        }
+        delete temp->next;
+
+    }
+};
